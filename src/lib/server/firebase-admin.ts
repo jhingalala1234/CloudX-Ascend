8a2a6b8a35e2c78aa0460fe6d1ffdebf90be5a62
CloudX - SRMIST <cloudx.srmist@gmail.com>
2025-09-10 05:41:14 +0000

    changed-code-files

--- src/lib/server/firebase-admin.ts
+++ src/lib/server/firebase-admin.ts
@@ -6,23 +6,24 @@ import serviceAccount from './google-credentials.json';
 
 let app: admin.app.App;
 
-try {
-  const privateKey = (serviceAccount as any).private_key.replace(/\\n/g, '\n');
+if (!admin.apps.length) {
+  try {
+    const privateKey = (serviceAccount as any).private_key.replace(/\\n/g, '\n');
 
-  app = admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: serviceAccount.project_id,
-      clientEmail: serviceAccount.client_email,
-      privateKey,
-    }),
-    storageBucket: `${serviceAccount.project_id}.appspot.com`,
-  });
-} catch (e: any) {
-  if (!/already exists/.test(e.message)) {
+    app = admin.initializeApp({
+      credential: admin.credential.cert({
+        projectId: serviceAccount.project_id,
+        clientEmail: serviceAccount.client_email,
+        privateKey,
+      }),
+      storageBucket: `${serviceAccount.project_id}.appspot.com`,
+    });
+  } catch (e: any) {
     console.error('Firebase admin initialization error', e.stack);
-  } else {
-    app = admin.app();
+    throw new Error('Failed to initialize Firebase Admin SDK.');
   }
+} else {
+  app = admin.app();
 }
 
 const firestore = admin.firestore(app);
